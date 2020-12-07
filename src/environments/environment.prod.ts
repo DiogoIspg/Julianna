a85ae1bc09eedfe5a7f51ab03a4b276601d78477
DiogoIspg <57219615+DiogoIspg@users.noreply.github.com>
2020-12-07 17:18:04 +0000

    Fixed env variables to point to new address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://https://scipark.azurewebsites.net/api'
+  apiUrl: 'https://scifind.azurewebsites.net/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://scipark.azurewebsites.net/api',
+  apiUrl: 'https://scifind.azurewebsites.net/api',
   // apiUrl: 'https://localhost:5001/api',
 };
